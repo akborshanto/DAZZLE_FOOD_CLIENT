9fd54d3e8946805a7101f971aba8e0b001a9c871
akbor shanto <akborshanto2002@gmail.com>
2024-05-12 10:53:13 -0700

    recomendation delete data

--- src/pages/error/Error.jsx
+++ src/pages/error/Error.jsx
@@ -1,20 +1,136 @@
 import React from 'react'
 import { Link} from "react-router-dom";
+import './err.css'
 const Errors = () => {
   return (
-    <div>
-    <section className="flex items-center h-full p-16 dark:bg-gray-50 dark:text-gray-800">
-    <div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
-      <div className="max-w-md text-center">
-        <h2 className="mb-8 font-extrabold text-9xl dark:text-gray-400">
-          <span className="sr-only">Error</span>404
-        </h2>
-        <p className="text-2xl font-semibold md:text-3xl">Sorry, we couldn't find this page.</p>
-        <p className="mt-4 mb-8 dark:text-gray-600">But dont worry, you can find plenty of other things on our homepage.</p>
-<Link to='/'>        <a rel="noopener noreferrer" href="#" className="px-8 py-3 font-semibold rounded dark:bg-violet-600 dark:text-gray-50">Back to homepage</a></Link>
+    <div className='mx-auto'>
+
+
+
+
+
+    <div class="main_wrapper">
+    <Link to='/'>
+
+<div class="w-full h-40 flex items-center justify-center cursor-pointer">
+  <div
+    class="relative inline-flex items-center justify-start py-3 pl-4 pr-12 overflow-hidden 
+    font-semibold shadow text-indigo-600 transition-all duration-150 ease-in-out rounded hover:pl-10 hover:pr-6 bg-white dark:bg-gray-700 dark:text-white dark:hover:text-gray-200 dark:shadow-none group"
+  >
+    <span
+      class="absolute bottom-0 left-0 w-full h-1 transition-all duration-150 ease-in-out bg-indigo-600 group-hover:h-full"
+    ></span>
+    <span
+      class="absolute right-0 pr-4 duration-200 ease-out group-hover:translate-x-12"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        fill="none"
+        class="w-5 h-5 text-green-400"
+      >
+        <path
+          d="M14 5l7 7m0 0l-7 7m7-7H3"
+          stroke-width="2"
+          stroke-linejoin="round"
+          stroke-linecap="round"
+        ></path>
+      </svg>
+    </span>
+    <span
+      class="absolute left-0 pl-2.5 -translate-x-12 group-hover:translate-x-0 ease-out duration-200"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        fill="none"
+        class="w-5 h-5 text-green-400"
+      >
+        <path
+          d="M14 5l7 7m0 0l-7 7m7-7H3"
+          stroke-width="2"
+          stroke-linejoin="round"
+          stroke-linecap="round"
+        ></path>
+      </svg>
+    </span>
+    <span
+      class="relative w-full text-left transition-colors duration-200 ease-in-out group-hover:text-white dark:group-hover:text-green-600"
+      >GO BACK HOME</span
+    >
+  </div>
+</div>
+
+</Link>
+    <div class="main">
+      <div class="antenna">
+        <div class="antenna_shadow"></div>
+        <div class="a1"></div>
+        <div class="a1d"></div>
+        <div class="a2"></div>
+        <div class="a2d"></div>
+        <div class="a_base"></div>
+      </div>
+      <div class="tv">
+        <div class="cruve">
+          <svg
+            xml:space="preserve"
+            viewBox="0 0 189.929 189.929"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+            xmlns="http://www.w3.org/2000/svg"
+            version="1.1"
+            class="curve_svg"
+          >
+            <path
+              d="M70.343,70.343c-30.554,30.553-44.806,72.7-39.102,115.635l-29.738,3.951C-5.442,137.659,11.917,86.34,49.129,49.13
+          C86.34,11.918,137.664-5.445,189.928,1.502l-3.95,29.738C143.041,25.54,100.895,39.789,70.343,70.343z"
+            ></path>
+          </svg>
+        </div>
+        <div class="display_div">
+          <div class="screen_out">
+            <div class="screen_out1">
+              <div class="screen">
+                <span class="notfound_text"> NOT FOUND</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      
+        <div class="lines">
+          <div class="line1"></div>
+          <div class="line2"></div>
+          <div class="line3"></div>
+        </div>
+        <div class="buttons_div">
+          <div class="b1"><div></div></div>
+          <div class="b2"></div>
+          <div class="speakers">
+            <div class="g1">
+              <div class="g11"></div>
+              <div class="g12"></div>
+              <div class="g13"></div>
+            </div>
+            <div class="g"></div>
+            <div class="g"></div>
+          </div>
+        </div>
+      </div>
+      <div class="bottom">
+        <div class="base1"></div>
+        <div class="base2"></div>
+        <div class="base3"></div>
       </div>
     </div>
-  </section>
+    <div class="text_404">
+      <div class="text_4041">4</div>
+      <div class="text_4042">0</div>
+      <div class="text_4043">4</div>
+    </div>
+  </div>
+  
     </div>
   )
 }
--- src/pages/myReqoomendation/MyRecomendation.jsx
+++ src/pages/myReqoomendation/MyRecomendation.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import useAuthHook from '../../firebase/authProvider/AuthHook';
 import axios from 'axios';
-import MyRecomendationTable from './MyRecomendationTable';
 
 const MyRecomendation = () => {
 const {user}=useAuthHook()
@@ -19,31 +18,193 @@ setMyReco(data)
 getData()
 },[])
 
-console.log(myReco)
+/* delete a specifinq fetchin */
+const handleDeleteReco=(id)=>{
+
+  fetch(`${import.meta.env.VITE_API_URL}/specifiqRecoDelete/${id}`,{
+
+
+    method:"DELETE",
+
+  })
+  .then(res=>res.json())
+  .then(data=>{
+
+//deleteed from ui
+if(data.deletedCount >0){
+
+const remaining=myReco.filter(D_UI=>{
+
+  D_UI._id !==id
+
+})
+setMyReco(remaining)
+
+
+ 
+}
+
+
+
+
+  })
+  
+
+
+// try{
+
+// const {data}=await axios.delete(`${import.meta.env.VITE_API_URL}/specifiqRecoDelete/${id}`)
+
+// console.log(data)
+
+
+// }catch(err){
+//   console.log('thii i ai err')
+// }
+
+
+
+
+//   console.log(id)
+}
+
   return (
     <div>
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <div className="overflow-x-auto">
     <table className="table">
-    <thead>
-    <tr>
-      <th>
-        <label>
-          <input type="checkbox" className="checkbox" />
-        </label>
-      </th>
-      <th>Name</th>
-      <th>Job</th>
-      <th>Favorite Color</th>
-      <th></th>
-    </tr>
-  </thead>
-
-  {
-    myReco.map(specifiqUser=><MyRecomendationTable specifiqUser={specifiqUser}></MyRecomendationTable>)
-  }
+      {/* head */}
+      <thead>
+        <tr>
+          <th>
   
-    </table>
+          </th>
+          <th>Name</th>
+          <th>Job</th>
+          <th>Favorite Color</th>
+          <th>Delete Recomendation</th>
+        </tr>
+      </thead>
+      <tbody>
 
+      {
+        myReco.map(specifiqUser=>
+          
+
+ (
+
+            <tr>
+            <th>
+             
+            </th>
+            <td>
+              <div className="flex items-center gap-3">
+                <div className="avatar">
+                  <div className="mask mask-squircle w-12 h-12">
+                    <img src={specifiqUser.R_PdPhoto} alt="Avatar Tailwind CSS Component" />
+                  </div>
+                </div>
+                <div>
+                  <div className="font-bold">{specifiqUser.pd_Name}</div>
+                  <div className="text-sm opacity-50">{specifiqUser.R_title}</div>
+                </div>
+              </div>
+            </td>
+            <td>
+           {specifiqUser.current_Name}
+              <br/>
+              <span className="badge badge-ghost badge-sm">{specifiqUser.curren_Email}</span>
+            </td>
+            <td>{specifiqUser.current_Time}</td>
+            <th>
+            <button
+            class="relative border-2 border-black group hover:border-green-500 w-12 h-12 duration-500 overflow-hidden"
+            type="button" onClick={()=>handleDeleteReco(specifiqUser._id)}
+          >
+            <p
+              class="font-Manrope text-3xl h-full w-full flex items-center justify-center text-white duration-500 relative z-10 group-hover:scale-0" 
+            >
+              X
+            </p>
+            <span
+              class="absolute w-full h-full bg-green-500 rotate-45 group-hover:top-9 duration-500 top-12 left-0"
+            ></span>
+            <span
+              class="absolute w-full h-full bg-green-500 rotate-45 top-0 group-hover:left-9 duration-500 left-12"
+            ></span>
+            <span
+              class="absolute w-full h-full bg-green-500 rotate-45 top-0 group-hover:right-9 duration-500 right-12"
+            ></span>
+            <span
+              class="absolute w-full h-full bg-green-500 rotate-45 group-hover:bottom-9 duration-500 bottom-12 right-0"
+            ></span>
+          </button>
+          
+            </th>
+          </tr>
+         
+  
+  
+          )
+
+        
+   
+   
+
+
+
+        
+
+          
+
+        )
+      }
+
+
+
+
+
+        {/* row 1 */}
+    
+      </tbody>
+      {/* foot */}
+      <tfoot>
+      
+      </tfoot>
+      
+    </table>
+  </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
 
 
 
